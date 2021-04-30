f9c4bf3594981230f6bf9511c15b0335afbc42d7
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-04-30 22:37:27 +0800

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,12 +1,5 @@
 var room_magnitude=5;
-var chat_room="
-<div class='chat-room'>
-    <img id='chat-header'src='../resources/nav/create_chat.png'/>
-    <div class='chat-room-text'>
-    <h4 id='chat-group-name'>鄭青宇</h4>
-    <h5 id='chat-firstline'>哈哈哈哈哈哈哈哈</h5>
-</div>
-</div>";
+var chat_room="<div class='chat-room'><img id='chat-header'src='../resources/nav/create_chat.png'/><div class='chat-room-text'><h4 id='chat-group-name'>鄭青宇</h4><h5 id='chat-firstline'>哈哈哈哈哈哈哈哈</h5></div></div>";
 function appendrooms(){
     for(var i=0;i<room_magnitude;i++){
         $("#chat-record").append("chatroom")
