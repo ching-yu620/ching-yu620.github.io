c1baeb157360af4af2c9a669ddfc138565a9ac20
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-04-30 21:16:58 +0800

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,6 +1,6 @@
-$(document).ready(function() => {
+$(document).ready(function(){
     // Navbar icon actions
-        $("#create-chat-button").(click(function () => {
+        $("#create-chat-button").(click(function (){
             console.log("create chat");
             $("create-chat-menu").removeClass("hidden").addClass("show");
         });
