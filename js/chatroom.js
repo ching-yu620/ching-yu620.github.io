1ec677e192d426d82dfb89da48bb79f7e340ff05
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-04-30 21:59:41 +0800

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,3 +1,18 @@
+var room_magnitude=5;
+function appendrooms(){
+    for(var i=0;i<room_magnitude;i++){
+        $("#chat-record").append(".chat-record")
+    }
+}
+$(.chat-room-text).ready(function(){
+    appendrooms();
+    $.post("demo_test_post.asp", {
+        chatroomID:
+    },
+    function(header,room_name,first_message){
+    
+  });
+});
 $(document).ready(function(){
     // Navbar icon actions
         $("#create-chat-button").click(function (){
