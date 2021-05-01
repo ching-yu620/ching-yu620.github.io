53e1aa14e0684e42fa173b6f2f655831c56ec83f
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-05-01 17:58:00 +0800

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -1,8 +1,9 @@
 var room_magnitude=5;
-var friends_magnitude=5;
+var friend_magnitude=5;
 var mission_magnitude=5;
 var chatroom="<div class='chat-room'><img id='chat-header'src='../resources/nav/create_chat.png'/><div class='chat-room-text'><h4 id='chat-group-name'>鄭青宇</h4><h5 id='chat-firstline'>哈哈哈哈哈哈哈哈</h5></div></div>";
 var header_name,group_name,firstline;
+var friends,missions;
 function modify_chatroom(){
     chatroom="<div class='chat-room'><img id='chat-header'src="+header_name+"/><div class='chat-room-text'><h4 id='chat-group-name'>"+group_name+"</h4><h5 id='chat-firstline'>+firstline+</h5></div></div>";
 }
@@ -11,8 +12,21 @@ function appendrooms(){
         $("#chat-record").append(chatroom)
     }
 }
-$("#chat-record").ready(function(){
-    appendrooms();
+function appendmissions(){
+    for(var i=0;i<mission_magnitude;i++){
+        $("#chat-choose-missions").append(missions)
+    }
+}
+function appendfriends(){
+    for(var i=0;i<friend_magnitude;i++){
+        $("#chat-choose-friends").append(friends)
+    }
+}
+$("#chat-choose-missions").ready(function(){
+    appendmissionss();
+});
+$("#chat-choose-friends").ready(function(){
+    appendfriendss();
 });
 $(".chat-room-text").ready(function(){
     $.post("demo_test_post.asp", {
@@ -27,7 +41,7 @@ $(document).ready(function(){
     // Navbar icon actions
         $("#create-chat-button").click(function (){
             console.log("create chat");
-            $("#chat-choose-mission").removeClass("hidden").addClass("show");
+            $("#chat-choose-missions").removeClass("hidden").addClass("show");
             //$(".choosed-mission").removeClass("hidden").addClass("show");
             $(".chat-cover").removeClass("hidden").addClass("show");
         });
