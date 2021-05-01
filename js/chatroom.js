53aa91efc16060b25cb8d926e64ea3e7d29318aa
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-05-01 21:41:54 +0800

    Update chatroom.js

--- js/chatroom.js
+++ js/chatroom.js
@@ -4,8 +4,8 @@ var mission_magnitude=5;
 
 var chatroom="<div class='chat-room'><img id='chat-header'src='../resources/nav/create_chat.png'/><div class='chat-room-text'><h4 id='chat-group-name'>鄭青宇</h4><h5 id='chat-firstline'>哈哈哈哈哈哈哈哈</h5></div></div>";
 var header_name,group_name,firstline;
-var missions="<input type='radio' name='choose_mission' id='cheese'><label for='cheese'><div class='choosed-mission unchosen'><h3>和陌生的你夜衝</h3></div></label>";
-var friends;
+var missions="<input type='radio' name='choose_mission' id='C_M'><label for='C_M'><div class='choosed-mission unchosen'><h3>和陌生的你夜衝</h3></div></label>";
+var friends="<input type='checkbox' name='choose_friend' id='C_F'><label for='C_F'><div class='choosed-friend unchosen'><img src='../resources/nav/create_chat.png'/><h3>鄭青宇</h3></div>;
 function choose_mission(){
     var obj=document.getElementsByName("choose_mission");
     var len = obj.length;
@@ -14,6 +14,17 @@ function choose_mission(){
             console.log(len+"missions");
             $(".choosed-mission[i]").removeClass("unchosen").addClass("chosen");
             $(".button-sure").removeClass("hidden").addClass("show");
+        }
+    }
+}
+function choose_friend(){
+    var obj=document.getElementsByName("choose_friend");
+    var len = obj.length;
+    for(var i=len-1;i>=0;i--){
+        if(obj[i].checked==true){
+            console.log(len+"missions");
+            $(".choosed-friend[i]").removeClass("unchosen").addClass("chosen");
+            $(".button-creategroup").removeClass("hidden").addClass("show");
         }
     }
 }
@@ -73,6 +84,7 @@ $(document).ready(function(){
      $(".button-sure").click(function (){
        $("#chat-choose-missions").removeClass("show").addClass("hidden");
        $("#chat-choose-friends").removeClass("hidden").addClass("show");
+       $(".button-sure").removeClass("show").addClass("hidden");
     });
 });
 
