5bb0322d345c3120f7c7b18801c9d8583cdf9f54
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-05-03 02:14:18 +0800

    Update chingyu.js

--- js/chingyu.js
+++ js/chingyu.js
@@ -41,7 +41,7 @@ function choose_mission(){
 function choose_friend(){
     var obj=document.getElementsByName("choose_friend");
     var len = obj.length;
-    for(var i=len-1;i>=0;i--){
+    for(var i=0;i<len;i++){
         if(obj[i].checked==true){
             console.log("the"+i+"checkbox true");
         }
