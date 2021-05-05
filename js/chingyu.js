d6e70f73ef53041575c9d01a1db992ae9bc7f489
ching-yu <46028943+ching-yu620@users.noreply.github.com>
2021-05-05 21:47:30 +0800

    Update chingyu.js

--- js/chingyu.js
+++ js/chingyu.js
@@ -24,16 +24,17 @@ var myname="鄭青宇";
       {name:"鄭青宇",msg:"jiooooooooooooooooooooooooooo",time:"06:48",image:"../resources/nav/create_chat.png"},
       {name:"莊小萱",msg:"weffffffffffffffffffffffffefw",time:"07:48",image:"../resources/nav/create_chat.png"},
       {name:"鄭青宇",msg:"jiooooooooooooooooooooooooooo",time:"06:48",image:"../resources/nav/create_chat.png"},
-      {name:"莊小萱",msg:"weffffffffffffffffffffffffefw",time:"07:48",image:"../resources/nav/create_chat.png"},
+      {name:"許育騰",msg:"weffffffffffffffffffffffffefw",time:"07:48",image:"../resources/nav/create_chat.png"},
       {name:"鄭青宇",msg:"jiooooooooooooooooooooooooooo",time:"06:48",image:"../resources/nav/create_chat.png"},
       {name:"莊小萱",msg:"周詩鏇周詩鏇周詩鏇周詩鏇周詩鏇周詩鏇",time:"07:48",image:"../resources/nav/create_chat.png"},
       {name:"鄭青宇",msg:"jiooooooooooooooooooooooooooo",time:"06:48",image:"../resources/nav/create_chat.png"},
-      {name:"莊小萱",msg:"周詩鏇周詩鏇周詩鏇周詩鏇周詩鏇",time:"07:48",image:"../resources/nav/create_chat.png"},
+      {name:"許育騰",msg:"周詩鏇周詩鏇周詩鏇周詩鏇周詩鏇",time:"07:48",image:"../resources/nav/create_chat.png"},
       {name:"鄭青宇",msg:"jiooooooooooooooooooooooooooo",time:"06:48",image:"../resources/nav/create_chat.png"},
       {name:"莊小萱",msg:"weffffffffffffffffffffffffefw",time:"07:48",image:"../resources/nav/create_chat.png"},
       {name:"鄭青宇",msg:"周詩鏇周詩鏇周詩鏇周詩鏇周詩鏇周詩鏇",time:"06:48",image:"../resources/nav/create_chat.png"},
       {name:"莊小萱",msg:"weffffffffffffffffffffffffefw",time:"07:48",image:"../resources/nav/create_chat.png"},
     ];
+var value={social:50;travel:45;food:23;activity:20;sport:15;self:10;}
 //some text need to be modified by variable
 
 
